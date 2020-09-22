b8e8c37ea26d6e76da684cdd88adcce9c4844259
Jose Daniel Morales Ocampo <jose-daniel-97@hotmail.com>
2020-09-22 18:44:00 -0500

    pluvify stable version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Pluvify - Cloud';
+  title = 'Pluvify Cloud';
   showFiller = false;
   isAuth = false;
   constructor(public auth: AuthService){
